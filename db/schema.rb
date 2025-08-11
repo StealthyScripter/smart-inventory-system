859c6d8ade0960b93bfab31523f76c1de597f332
StealthyScripter <wendotbrian@gmail.com>
2025-08-11 17:23:11 -0400

    Created users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_11_190937) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_11_191002) do
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
     t.text "description"
@@ -37,5 +37,18 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_11_190937) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "role", null: false
+    t.integer "location_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["location_id"], name: "index_users_on_location_id"
+  end
+
   add_foreign_key "locations", "users", column: "manager_id"
+  add_foreign_key "users", "locations"
 end
