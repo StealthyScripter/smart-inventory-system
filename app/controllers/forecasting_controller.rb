3c580116dfc8242d75d2b16383b001de1c619336
StealthyScripter <wendotbrian@gmail.com>
2025-09-17 11:09:01 -0400

    Feat: modified the controllers

--- app/controllers/forecasting_controller.rb
+++ app/controllers/forecasting_controller.rb
@@ -1,4 +1,7 @@
-def index
-  @forecasts = DemandForecast.includes(:product, :location)
-    .where("forecast_date >= ?", Date.current)
+class ForecastingController < ApplicationController
+  def index
+    @forecasts = DemandForecast.includes(:product, :location)
+                               .where("forecast_date >= ?", Date.current)
+                               .order(:forecast_date)
+  end
 end
--- app/controllers/purchase_orders_controller.rb
+++ app/controllers/purchase_orders_controller.rb
@@ -1,4 +1,10 @@
-def index
-  @purchase_orders = PurchaseOrder.includes(:supplier, :user)
-    .order(created_at: :desc)
+class PurchaseOrdersController < ApplicationController
+  def index
+    @purchase_orders = PurchaseOrder.includes(:supplier, :user)
+      .order(created_at: :desc)
+  end
+
+  def show
+    @purchase_order = PurchaseOrder.find(params[:id])
+  end
 end
