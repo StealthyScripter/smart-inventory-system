6eda78c38f3249c49066ad271181989a207e5557
StealthyScripter <wendotbrian@gmail.com>
2025-09-17 18:18:48 -0400

    Added links to the different pages

--- app/controllers/forecasting_controller.rb
+++ app/controllers/forecasting_controller.rb
@@ -1,7 +1,7 @@
 class ForecastingController < ApplicationController
   def index
     @forecasts = DemandForecast.includes(:product, :location)
-                               .where("forecast_date >= ?", Date.current)
-                               .order(:forecast_date)
+          .where("forecast_date >= ?", Date.current)
+          .order(:forecast_date)
   end
 end
